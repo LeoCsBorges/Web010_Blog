01c2193c3be26cf4bd01d048489022618953acba
Léo Borges <leo_borgess@live.com>
2024-12-18 15:45:53 -0300

    adding home page version 1.0

--- app/main.js
+++ app/main.js
@@ -2,13 +2,22 @@ import { getPosts } from "./api/posts.js";
 import { renderPosts } from "./view/posts.js";
 
 document.addEventListener('DOMContentLoaded', async function () {
-    const posts = await getPosts();
-
-    const postsElements = renderPosts(posts);
-
-    document.querySelector('section.posts').append(...postsElements);
+    // const posts = await getPosts();
+    // const postsElements = renderPosts(posts);
+    // document.querySelector('section.posts').append(...postsElements);
     // const postsSection = document.querySelector('section.posts');
     // postsSection.append.apply(postsSection, postsElements);
+    const date = new Date().toLocaleString('pt-BR', {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+    });
+    // date.toLocaleDateString('pt-BR', {
+    //     day: "numeric",
+    // })
+
+    document.querySelector('.timing').innerHTML = date;
 })
 
 
